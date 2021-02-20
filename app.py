526d1a35d79757d77969af73f3985529d45e8333
Anthony Jiang <anthony.jiang@yale.edu>
2021-02-20 17:22:05 -0500

    documentation, UI improvements, PEP8

--- app.py
+++ app.py
@@ -1,78 +1,79 @@
+"""
+App interface. This is entirely frontend; all encryption logic is called from the decipher module
+
+"""
 import streamlit as st
 import decipher
 
 
 cipher_list = ['Atbash Cipher', 'Ceasar Cipher']
 
-def dec_wrap(func, *arg, **kwargs):
+
+def dec_wrap(func: callable, *arg, **kwargs) -> str:
     """
     A wrapper to check for value errors. I don't know if this is a good design choice, but it allows me to 
     1. decouple error decipher error handling from streamlit error reporting
     2. avoid repeating the same try-except four times
 
+    Args:
+        func: cipher function to call and wrap
+        *args: args to pass to the cipher function
+        **kwargs: keyword args to pass to the cipher function
+
+    Returns:
+        The decrypted or encrypted string
+
     """
     try:
         return func(*arg, **kwargs)
     except ValueError:
-        st.warning("Make sure your secret message only includes numbers, letters, and punctuation!")
+        st.warning(
+            "Make sure your secret message only includes numbers, letters, and punctuation!")
 
 
 def run():
     """
-    Main run method.
-
+    Main run method. 
     """
-    st.header("🔑 Decipher")
+    st.header("🔑 Decrypt a secret message...")
 
-    st.write("Decipher any string using a Atbash or Ceasarean cipher!")
+    st.subheader("Decrypt any string using an Atbash or Ceasarean cipher!")
+    st.write("Enter your encrypted text and hit enter.")
+    str_in = st.text_input("String to decrypt", "Svool!")
 
-    str_in = st.text_input("String to decipher", "Ifmmp!")
+    dec_cipher = st.selectbox("Cipher to use", cipher_list, key="dec")
 
-    dec_cipher = st.selectbox("Cipher to use", cipher_list, key= "dec")
+    if dec_cipher == cipher_list[0]:
+        output = dec_wrap(decipher.dec_atbash, str_in)
+        st.write(f"🔑 Decrypted text: **{output}**")
 
-    if dec_cipher == 'Ceasar Cipher':
+    if dec_cipher == cipher_list[1]:
         st.write("The rotation refers to the rotation used to encrypt. Thus, if you select 3, you are trying to undo a rotation of 3, which means you want to decrypt 'd' into 'a'")
         rot = st.slider("Cipher Rotation", 0, 26, 13, 1, key='dec')
+        # 26-rot gives the corresponding decryption to rot
+        output = dec_wrap(decipher.dec_ceasar, str_in, 26 - rot)
+        st.write(f"🔑 Decrypted text: **{output}**")
 
-    decrypt = st.button("Decipher!")
+    st.header("🔒 Encrypt a secret message...")
 
-    if decrypt:
-        if dec_cipher == 'Atbash Cipher':
-                output = dec_wrap(decipher.dec_atbash, str_in) 
-                st.write(f"Deciphered text: {output}") 
-
-        elif dec_cipher == "Ceasar Cipher":
-                output = dec_wrap(decipher.dec_ceasar, str_in, 26 - rot)
-                st.write(f"Deciphered text: {output}")
-
-        
-    st.header("🔒 Encrypt")
-
-    st.write("Encrypt any string using a Atbash or Ceasarean cipher!")
-
+    st.subheader("Encrypt any string using an Atbash or Ceasarean cipher!")
+    st.write("Enter the message you want to encrypt, and hit enter.")
     str_in = st.text_input("String to encrypt", "Hello!")
 
-    enc_cipher = st.selectbox("Cipher to use", cipher_list, key = "enc")
+    enc_cipher = st.selectbox("Cipher to use", cipher_list, key="enc")
 
-    if enc_cipher == "Atbash Cipher":
+    if enc_cipher == cipher_list[0]:
         st.write("The Atbash Cipher maps the alphabet to it's reverse, so 'a' becomes 'z' and vice versa. This implementation preserves case in its mapping, does not map numbers, spaces, or punctuation, and will not map any non-Ascii character.")
+        output = dec_wrap(decipher.dec_atbash, str_in)
+        st.write(f"🔒 Encrypted text: **{output}**")
 
-    if enc_cipher == 'Ceasar Cipher':
+    if enc_cipher == cipher_list[1]:
         st.write("The Ceasar Cipher maps the alphabet to a 'rotation' of the alphabet. In this way, with a Ceasar cipher with rotation of 3 would map 'a' to 'd'. This implementation preserves case, does not map numbers, spaces, or punctuation, and will not map any non-Ascii character.")
 
         rot = st.slider("Cipher Rotation", 0, 26, 13, 1, key='enc')
 
-    encrypt = st.button("Encrypt!")
-
-    if encrypt:
-        if enc_cipher == 'Atbash Cipher':
-                output = dec_wrap(decipher.dec_atbash, str_in)
-                st.write(f"Encrypted text: {output}")
-
-        elif enc_cipher == "Ceasar Cipher":
-
-                output = dec_wrap(decipher.dec_ceasar, str_in, rot) 
-                st.write(f"Encrypted text: {output}")
+        output = dec_wrap(decipher.dec_ceasar, str_in, rot)
+        st.write(f"🔒 Encrypted text: **{output}**")
 
 
 if __name__ == "__main__":
--- decipher.py
+++ decipher.py
@@ -1,10 +1,17 @@
+"""
+Decryption/encryption module. Provides utilities for decrypting atbash and caesar ciphers, as well as a hidden utility
+for manually determining the appropriate caesar cipher key for decryption. 
+
+Completely unbundled from streamlit, so any other frontend framework can be used. 
+
+"""
 import string
 
 string_1 = "Sld wl blf hloev zmb vjfzgrlm?"
 string_2 = "Zhygvcyl obgu fvqrf ol mreb."
 
 
-def dec_atbash(str_in: str)->str:
+def dec_atbash(str_in: str) -> str:
     """
     Applies the atbash cipher to str_in. Applying an atbash cipher is equivalent to 
     reversing the atbash cipher. It applies a different cipher to uppercase and lowercase values.
@@ -24,23 +31,24 @@ def dec_atbash(str_in: str)->str:
     new_str = []
     for index, char in enumerate(str_in):
         new_char = None
-        if char.isalpha(): 
+        if char.isalpha():
             char_ascii = ord(char)
-            if char_ascii > ord('Z'):  
+            if char_ascii > ord('Z'):
                 new_char = chr(ord('z') - (char_ascii - ord('a')))
             else:
                 new_char = chr(ord('Z') - (char_ascii - ord('A')))
         elif char in string.punctuation or char == ' ' or char.isnumeric():
             new_char = char
         else:
-            raise ValueError(f"{char} at index {index} could not be deciphered.")
+            raise ValueError(
+                f"{char} at index {index} could not be deciphered.")
 
         new_str.append(new_char)
 
-    
     return ''.join(new_str)
 
-def dec_ceasar(str_in:str, rot: int)->str:
+
+def dec_ceasar(str_in: str, rot: int) -> str:
     """
     Performs a ceasaraian rotation, adding rot to every char's ascii then taking (x - 68) (mod 26) + 68. 
     It applies the same rotation constant for uppercase and lowercase results. Does not rotate punctuation. 
@@ -63,34 +71,33 @@ def dec_ceasar(str_in:str, rot: int)->str:
         new_char = None
         if char.isalpha():
             char_ascii = ord(char)
-            if char_ascii > ord('Z'):  
-                new_char = chr(((char_ascii + rot) - ord('a'))%26 + ord('a'))
+            if char_ascii > ord('Z'):
+                new_char = chr(((char_ascii + rot) - ord('a')) % 26 + ord('a'))
             else:
-                new_char = chr(((char_ascii + rot) - ord('A'))%26 + ord('A'))
+                new_char = chr(((char_ascii + rot) - ord('A')) % 26 + ord('A'))
         elif char in string.punctuation or char == ' ' or char.isnumeric():
             new_char = char
         else:
-            raise ValueError(f"{char} at index {index} could not be deciphered.")
+            raise ValueError(
+                f"{char} at index {index} could not be deciphered.")
 
         new_str.append(new_char)
 
-
     return ''.join(new_str)
 
-def _find_ceasar_rot(str_in: str)->None:
+
+def _find_ceasar_rot(str_in: str) -> None:
     """
     Rotates the string all 26 ways for me to determine the appropriate rotation. Prints all possibilities
 
     Args:
         str_in: the sample string to rotate
-    
+
     """
 
     for rot in range(26):
         str_res = dec_ceasar(str_in, rot)
         print(f"Rotation {rot}: {str_res}")
-
-
 
 
 if __name__ == "__main__":
